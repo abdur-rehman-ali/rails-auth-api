40f7cb2ea4461814e77da6b1785774da170af23a
abdur-rehman-ali <alijoyia911@gmail.com>
2023-03-26 22:21:19 +0500

    Add model level validation on email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,9 @@
 #  username               :string           not null
 #
 class User < ApplicationRecord
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
+  
   include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: self
